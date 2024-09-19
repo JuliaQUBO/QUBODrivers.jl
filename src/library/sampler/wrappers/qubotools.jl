9efd9f76eba55c20ba4b2b74793ae7b231c1e53d
Pedro Maciel Xavier <pmacielx@purdue.edu>
2024-09-19 10:51:20 -0400

    Add method `MOI.get(_, ::QUBOTools_MOI.NumberOfReads)`

--- src/library/sampler/wrappers/qubotools.jl
+++ src/library/sampler/wrappers/qubotools.jl
@@ -1,1 +1,13 @@
+function MOI.get(sampler::AbstractSampler{T}, nr::QUBOTools_MOI.NumberOfReads) where {T}
+    i = nr.result_index
+    ω = QUBOTools.solution(sampler)
+    m = length(ω)
 
+    if isempty(ω)
+        error("Invalid result index '$i'; There are no solutions")
+    elseif !(1 <= i <= m)
+        error("Invalid result index '$i'; There are $(m) solutions")
+    end
+
+    return QUBOTools.reads(ω, i)
+end
