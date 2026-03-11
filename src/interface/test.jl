f65f6c9b0be27d2e270b4c14e230cc6271747545
David E. Bernal Neira <dbernaln@purdue.edu>
2026-03-10 22:55:25 -0400

    Align QUBODrivers.test doc defaults

--- src/interface/test.jl
+++ src/interface/test.jl
@@ -1,5 +1,5 @@
 @doc raw"""
-    test(optimizer::Type{S}; examples::Bool=false) where {S<:AbstractSampler}
-    test(config!::Function, optimizer::Type{S}; examples::Bool=false) where {S<:AbstractSampler}
+    test(optimizer::Type{S}; examples::Bool=true) where {S<:AbstractSampler}
+    test(config!::Function, optimizer::Type{S}; examples::Bool=true) where {S<:AbstractSampler}
 """
 function test end
