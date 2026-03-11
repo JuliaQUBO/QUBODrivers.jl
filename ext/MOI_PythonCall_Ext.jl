2dfa8f0b07165ada48506d72105201ae02b081e7
David E. Bernal Neira <dbernaln@purdue.edu>
2026-03-11 10:14:29 -0400

    Fix MOI PythonCall extension comment

--- ext/MOI_PythonCall_Ext.jl
+++ ext/MOI_PythonCall_Ext.jl
@@ -7,4 +7,4 @@ if !hasmethod(MOI.Utilities.map_indices, Tuple{<:Function,PythonCall.Py})
     MOI.Utilities.map_indices(::Function, obj::PythonCall.Py) = obj
 end
 
-end # module MOI_PythonCall
+end # module MOI_PythonCall_Ext
