89dc70d8bb630d6018795433be3e9494bc888983
David E. Bernal Neira <dbernaln@purdue.edu>
2026-03-10 22:03:35 -0400

    Handle fixed-variable VariablePrimalStart access

--- src/library/setup/quote.jl
+++ src/library/setup/quote.jl
@@ -227,32 +227,12 @@ function __setup_quote_moi_attrs(spec::_SamplerSpec)
 
         # MOI.VariablePrimalStart - get
         function MOI.get(sampler::$(Optimizer), ::MOI.VariablePrimalStart, vi::VI)
-            i = QUBOTools.index(sampler, vi)
-
-            return QUBOTools.start(sampler, i)
+            return QUBODrivers._get_variable_primal_start(sampler, vi)
         end
 
         # MOI.VariablePrimalStart - set
         function MOI.set(sampler::$(Optimizer){T}, ::MOI.VariablePrimalStart, vi::VI, value) where {T}
-            if !(isnothing(value) || value isa Real)
-                error("Value for 'MOI.VariablePrimalStart' must be an integer, or 'nothing'")
-            end
-
-            if !isnothing(value)
-                if !(value isa Real && isinteger(value))
-                    error("Value for 'MOI.VariablePrimalStart' must be an integer, or 'nothing'")
-                end
-
-                X = QUBOTools.domain(sampler)
-
-                if X === QUBOTools.BoolDomain && !(value == zero(value) || value == one(value))
-                    error("Integer value for 'MOI.VariablePrimalStart' must be either '0' or '1'")
-                elseif X === QUBOTools.SpinDomain && !(value == -one(value) || value == one(value))
-                    error("Integer value for 'MOI.VariablePrimalStart' must be either '-1' or '1'")
-                end
-            end
-
-            QUBOTools.attach!(sampler, vi => convert(Union{Integer, Nothing}, value))
+            QUBODrivers._set_variable_primal_start!(sampler, vi, value)
 
             return nothing
         end
